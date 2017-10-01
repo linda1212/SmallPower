24fd827e1c8283092918b819a4645b3773fc0cf9
psw1212@163.com <!QAZ2wsx>
2017-10-01 13:36:24 +0800

    導入功能完善

--- html/js/import.js
+++ html/js/import.js
@@ -1,18 +1,72 @@
-var baseurl = "js";
-document.write("<script language=\"javascript\" src=\"" + baseurl + "\/jquery.min.js\"></script>");
-document.write("<script language=\"javascript\" src=\"" + baseurl + "\/pages\/eBase.js\"></script>");
-document.write("<script language=\"javascript\" src=\"" + baseurl + "\/bootstrap.min.js\"></script>");
-document.write("<script language=\"javascript\" src=\"" + baseurl + "\/plugins\/dataTables\/jquery.dataTables.js\"></script>");
-document.write("<script language=\"javascript\" src=\"" + baseurl + "\/plugins\/dataTables\/dataTables.bootstrap.js\"></script>");
-document.write("<script language=\"javascript\" src=\"" + baseurl + "\/plugins\/treeview\/bootstrap-treeview.js\"></script>");
-document.write("<script language=\"javascript\" src=\"" + baseurl + "\/plugins\/metisMenu\/jquery.metisMenu.js\"></script>");
-document.write("<script language=\"javascript\" src=\"" + baseurl + "\/plugins\/slimscroll\/jquery.slimscroll.min.js\"></script>");
-document.write("<script language=\"javascript\" src=\"" + baseurl + "\/plugins\/layer\/layer.min.js\"></script>");
-document.write("<script language=\"javascript\" src=\"" + baseurl + "\/plugins\/pace\/pace.min.js\"></script>");
-document.write("<script language=\"javascript\" src=\"" + baseurl + "\/plugins\/echarts\/echarts-all.js\"></script>");
-document.write("<script language=\"javascript\" src=\"" + baseurl + "\/demo\/treeview-demo.min.js\"></script>");
-document.write("<script language=\"javascript\" src=\"" + baseurl + "\/demo\/echarts-demo.min.js\"></script>");
-document.write("<script language=\"javascript\" src=\"" + baseurl + "\/startDataTable.js\"></script>");
-document.write("<script language=\"javascript\" src=\"" + baseurl + "\/hplus.min.js\"></script>");
-document.write("<script language=\"javascript\" src=\"" + baseurl + "\/contabs.min.js\"></script>");
-console.log("[import.js][load js files end]");
+window.jsConfig = {
+    jsBasePath: 'js',
+    jQueryUrl: [{
+        url: 'jquery.min.js'
+    }],
+    jsUrls: [ {
+        url: 'jquery.min.js'
+    },{
+        url: 'pages/eBase.js'
+    }, {
+        url: 'vue.js'
+    }, {
+        url: 'bootstrap.min.js'
+    }, {
+        url: 'plugins/metisMenu/jquery.metisMenu.js'
+    }, {
+        url: 'plugins/slimscroll/jquery.slimscroll.min.js'
+    }, {
+        url: 'plugins/layer/layer.min.js'
+    }, {
+        url: 'plugins/dataTables/jquery.dataTables.js'
+    }, {
+        url: 'plugins/dataTables/dataTables.bootstrap.js'
+    }, {
+        url: 'plugins/treeview/bootstrap-treeview.js'
+    }, {
+        url: 'demo/treeview-demo.min.js'
+    }, {
+        url: 'startDataTable.js'
+    }, {
+        url: 'hplus.min.js'
+    }, {
+        url: 'contabs.min.js'
+    }, {
+        url: 'plugins/pace/pace.min.js'
+    }, {
+        url: 'plugins/echarts/echarts-all.js'
+    }, {
+        url: 'demo/echarts-demo.min.js'
+    }],
+    importJSFilesByList: function (list) {
+        var self = this;
+        for (var i = 0; i < list.length; i++) {
+            document.write('<script language="javascript" src="' + self.jsBasePath + '\/' + list[i].url + '"></script>');
+        }
+    },
+    importJQuery: function () {
+        this.importJSFilesByList(this.jQueryUrl);
+    },
+    importJSFiles: function () {
+        this.importJSFilesByList(this.jsUrls);
+    },
+    importOnlyBootstrap:function(){
+        this.importJQuery();
+    },
+    importOnlyEChart:function(){
+
+    },
+    importBoostrapAndEChart:function(){
+
+    },
+
+    importIndepent:function(){
+
+    }
+};
+
+window.jsConfig.importJSFiles();
+
+
+
+
