f0861d16f55cc9b70a6963e9ea56c470b7faf446
psw1212@163.com <!QAZ2wsx>
2017-10-01 19:13:51 +0800

    同步esp

--- html/js/import.js
+++ html/js/import.js
@@ -1,42 +1,28 @@
 window.jsConfig = {
     jsBasePath: 'js',
-    jQueryUrl: [{
+    baseUrls: [{
+        //base start
         url: 'jquery.min.js'
-    }],
-    jsUrls: [ {
-        url: 'jquery.min.js'
-    },{
+    }, {
         url: 'pages/eBase.js'
     }, {
         url: 'vue.js'
     }, {
         url: 'bootstrap.min.js'
     }, {
-        url: 'plugins/metisMenu/jquery.metisMenu.js'
+        //tree view start
+        url: 'plugins/treeview/bootstrap-treeview.js'
     }, {
-        url: 'plugins/slimscroll/jquery.slimscroll.min.js'
+        url: 'demo/treeview-demo.min.js'
     }, {
-        url: 'plugins/layer/layer.min.js'
+        //dataTables
+        url: 'plugins/jeditable/jquery.jeditable.js'
     }, {
         url: 'plugins/dataTables/jquery.dataTables.js'
     }, {
         url: 'plugins/dataTables/dataTables.bootstrap.js'
     }, {
-        url: 'plugins/treeview/bootstrap-treeview.js'
-    }, {
-        url: 'demo/treeview-demo.min.js'
-    }, {
         url: 'startDataTable.js'
-    }, {
-        url: 'hplus.min.js'
-    }, {
-        url: 'contabs.min.js'
-    }, {
-        url: 'plugins/pace/pace.min.js'
-    }, {
-        url: 'plugins/echarts/echarts-all.js'
-    }, {
-        url: 'demo/echarts-demo.min.js'
     }],
     importJSFilesByList: function (list) {
         var self = this;
@@ -44,29 +30,9 @@ window.jsConfig = {
             document.write('<script language="javascript" src="' + self.jsBasePath + '\/' + list[i].url + '"></script>');
         }
     },
-    importJQuery: function () {
-        this.importJSFilesByList(this.jQueryUrl);
-    },
-    importJSFiles: function () {
-        this.importJSFilesByList(this.jsUrls);
-    },
-    importOnlyBootstrap:function(){
-        this.importJQuery();
-    },
-    importOnlyEChart:function(){
-
-    },
-    importBoostrapAndEChart:function(){
-
-    },
-
-    importIndepent:function(){
-
+    initNormalPages: function () {
+        this.importJSFilesByList(this.baseUrls);
     }
 };
 
-window.jsConfig.importJSFiles();
-
-
-
-
+window.jsConfig.initNormalPages();
